417331620ad6ad036c253d13685920ecc90746ac
micakn <marilenknass@gmail.com>
2025-10-21 10:30:02 -0300

    Update rutas

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'
-import './styles/variables.css';       //  colores y fuente
-import './styles/layout.css';          //  estructura general
-import './styles/IntegrantesBase.css'; //  base común entre integrantes
-import './styles/util.css';            // utilidades globales (cards, grids, etc.)
-import './styles/themes.css';
+
+// 🎨 Orden lógico de estilos
+import './styles/variables.css'      // 1. Variables globales
+import './index.css'                 // 2. Reset global
+import './styles/util.css'           // 3. Utilidades
+import './styles/IntegrantesBase.css'// 4. Bases componentes
+import './styles/themes.css'         // 5. Temas
+import './App.css'                   // 6. App principal
 
 
 // Renderizar la aplicación con React Router para SPA
